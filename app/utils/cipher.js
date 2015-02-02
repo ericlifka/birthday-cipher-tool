eb44993f7c41e2c281f9e8ab5a98f05190848e8e
Eric Lifka <eric.lifka@gmail.com>
2015-02-01 21:37:26 -0500

    cipher function

---
+++ app/utils/cipher.js
@@ -0,0 +1,3 @@
+export default function cipher() {
+  return true;
+}
---
+++ tests/unit/utils/cipher-test.js
@@ -0,0 +1,9 @@
+import cipher from 'birthday-cipher-tool/utils/cipher';
+
+module('cipher');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = cipher();
+  ok(result);
+});
