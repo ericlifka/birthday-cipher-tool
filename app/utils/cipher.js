89f464d4d3314a59778d35781c6f6b85e3c8d92f
Eric Lifka <eric.lifka@gmail.com>
2015-02-01 21:43:54 -0500

    create a translation map to use for ciphering text

--- app/utils/cipher.js
+++ app/utils/cipher.js
@@ -1,3 +1,8 @@
-export default function cipher() {
-  return true;
+var translationMap = {};
+"abcdefghijklmnopqrstuvwxyz".split('').forEach(function (letter, index) {
+    translationMap[letter] = index;
+});
+
+export default function cipher(text) {
+  return [text, text, text, text];
 }
