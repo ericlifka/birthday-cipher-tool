2a689281bd38ad81a23434eb5dbbf134a483d8b2
Eric Lifka <eric.lifka@gmail.com>
2015-02-01 21:33:57 -0500

    output goes in an array that all gets rendered

--- app/components/cipher-entry.js
+++ app/components/cipher-entry.js
@@ -3,9 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     classNames: ["cipher-entry"],
 
+    ciphers: null,
+
     textInput: Ember.computed(function () {
         return "";
     }),
 
-
+    runCipher: Ember.observer("textInput", function () {
+        var text = this.get('textInput');
+        this.set('ciphers', [text, text, text]);
+    })
 });
